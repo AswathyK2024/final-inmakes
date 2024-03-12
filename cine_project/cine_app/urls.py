4d2c7ce9307715b60d5a37158a01db3fae930150
Aswathy k <kaswathy2020@gmail.com>
2024-03-12 14:57:22 +0530

    final inmakes priject

---
+++ cine_project/cine_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from cine_app import views
+
+app_name = 'cine_app'
+urlpatterns = [
+    path('', views.allMoviedetails, name='allMoviedetails'),
+    path('<slug:c_slug>/', views.allMoviedetails, name='movie_details_by_category'),
+    path('<slug:c_slug>/<slug:movie_slug>/', views.detailedOfMovie, name='detailedOfMovie'),
+]
